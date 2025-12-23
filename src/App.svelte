20bba6034a08390fe51fe02a8b14750ee401671d
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-12-23 16:46:00 +0800

    Add skin complexion color picker - Implement color input with reactive binding - Display hex value in label - Default to #F5E0D3

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
   import Header from "./lib/Header.svelte";
   import GenderForm from "./lib/GenderForm.svelte";
   import MobileNumberForm from "./lib/MobileNumberForm.svelte";
+
+  import SkinComplexionForm from "./lib/SkinComplexionForm.svelte";
 </script>
 
 <main
@@ -10,4 +12,5 @@
   <Header />
   <GenderForm />
   <MobileNumberForm />
+  <SkinComplexionForm />
 </main>
---
+++ src/lib/SkinComplexionForm.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let color = $state("#F5E0D3");
+</script>
+
+<div class="flex flex-col w-full gap-2.5">
+  <label for="color">Skin Complexion: {color}</label>
+  <input bind:value={color} type="color" name="color" id="color" />
+</div>
